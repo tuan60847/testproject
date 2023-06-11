10a37fe6f813c3ad07731b792da9ad51b1e9e98d
ninh2102 <dh51905239@student.stu.edu.vn>
2023-06-11 15:11:50 +0700

    ninh

---
+++ resources/views/chukhachsan/hotelier.blade.php
@@ -0,0 +1,75 @@
+@extends('layout')
+@section('content')
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Khách hàng thân thiết</h1>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Khách hàng thân thiết</h6>
+        <!-- <a href="{{url('/createTP')}}" class="float-right btn-primary btn-sm">Thêm Mới</a> -->
+        </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>ADMINKS</th>
+                        <th>Họ Tên</th>
+                        <th>Ngày Sinh</th>
+                        <th>SDT</th>
+                        <th>CMND/CCCD</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                <tr>
+                        <th>ADMINKS</th>
+                        <th>Họ Tên</th>
+                        <th>Ngày Sinh</th>
+                        <th>SDT</th>
+                        <th>CMND/CCCD</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>System Architect</td>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>System Architect</td>
+                        <td>Tiger Nixon</td>
+                        <td>
+                            <a href="" class="btn btn-info btn-sm"><i class="fa fa-eye"></i>
+                            <a href="" class="btn btn-success btn-sm"><i class="fa fa-edit"></i>
+                            <a href="" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+@section('script')
+ <!-- Custom styles for this page -->
+ <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+ <!-- Page level plugins -->
+ <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+    @endsection
+@endsection
